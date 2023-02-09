b752b7a600f1d2a0b4ca750cb9091666d9bd569d
NOMADHACK <124717764+NOMADHACK@users.noreply.github.com>
2023-02-10 01:58:37 +0700

    Add files via upload

---
+++ update/Cheer Up/video-list.js
@@ -0,0 +1,82 @@
+let allVideos = [
+   {
+      name: "Cheer Up Ep1",
+      src: "cus1%20%281%29",
+      id: "vid_1"
+   },
+   {
+      name: "Cheer Up Ep2",
+      src: "cus1%20%282%29",
+      id: "vid_2"
+   },
+   {
+      name: "Cheer Up Ep3",
+      src: "cus1%20%283%29",
+      id: "vid_3"
+   },
+   {
+      name: "Cheer Up Ep4",
+      src: "cus1%20%284%29",
+      id: "vid_4"
+   },
+   {
+      name: "Cheer Up Ep5",
+      src: "cus1%20%285%29",
+      id: "vid_5"
+   },
+   {
+      name: "Cheer Up Ep6",
+      src: "cus1%20%286%29",
+      id: "vid_6"
+   },
+   {
+      name: "Cheer Up Ep7",
+      src: "cus1%20%287%29",
+      id: "vid_7"
+   },
+   {
+      name: "Cheer Up Ep8",
+      src: "cus1%20%288%29",
+      id: "vid_8"
+   },
+   {
+      name: "Cheer Up Ep9",
+      src: "cus1%20%289%29",
+      id: "vid_9"
+   },
+   {
+      name: "Cheer Up Ep10",
+      src: "cus1%20%2810%29",
+      id: "vid_10"
+   },
+   {
+      name: "Cheer Up Ep11",
+      src: "cus1%20%2811%29",
+      id: "vid_11"
+   },
+   {
+      name: "Cheer Up Ep12",
+      src: "cus1%20%2812%29",
+      id: "vid_12"
+   },
+   {
+      name: "Cheer Up Ep13",
+      src: "cus1%20%2813%29",
+      id: "vid_13"
+   },
+   {
+      name: "Cheer Up Ep14",
+      src: "cus1%20%2814%29",
+      id: "vid_14"
+   },
+   {
+      name: "Cheer Up Ep15",
+      src: "cus1%20%2815%29",
+      id: "vid_15"
+   },
+   {
+      name: "Cheer Up Ep16",
+      src: "cus1%20%2816%29",
+      id: "vid_16"
+   }
+]
---
+++ update/Curtain Call/video-list.js
@@ -0,0 +1,82 @@
+let allVideos = [
+   {
+      name: "Curtain Call Ep1",
+      src: "ccs1%20%281%29",
+      id: "vid_1"
+   },
+   {
+      name: "Curtain Call Ep2",
+      src: "ccs1%20%282%29",
+      id: "vid_2"
+   },
+   {
+      name: "Curtain Call Ep3",
+      src: "ccs1%20%283%29",
+      id: "vid_3"
+   },
+   {
+      name: "Curtain Call Ep4",
+      src: "ccs1%20%284%29",
+      id: "vid_4"
+   },
+   {
+      name: "Curtain Call Ep5",
+      src: "ccs1%20%285%29",
+      id: "vid_5"
+   },
+   {
+      name: "Curtain Call Ep6",
+      src: "ccs1%20%286%29",
+      id: "vid_6"
+   },
+   {
+      name: "Curtain Call Ep7",
+      src: "ccs1%20%287%29",
+      id: "vid_7"
+   },
+   {
+      name: "Curtain Call Ep8",
+      src: "ccs1%20%288%29",
+      id: "vid_8"
+   },
+   {
+      name: "Curtain Call Ep9",
+      src: "ccs1%20%289%29",
+      id: "vid_9"
+   },
+   {
+      name: "Curtain Call Ep10",
+      src: "ccs1%20%2810%29",
+      id: "vid_10"
+   },
+   {
+      name: "Curtain Call Ep11",
+      src: "ccs1%20%2811%29",
+      id: "vid_11"
+   },
+   {
+      name: "Curtain Call Ep12",
+      src: "ccs1%20%2812%29",
+      id: "vid_12"
+   },
+   {
+      name: "Curtain Call Ep13",
+      src: "ccs1%20%2813%29",
+      id: "vid_13"
+   },
+   {
+      name: "Curtain Call Ep14",
+      src: "ccs1%20%2814%29",
+      id: "vid_14"
+   },
+   {
+      name: "Curtain Call Ep15",
+      src: "ccs1%20%2815%29",
+      id: "vid_15"
+   },
+   {
+      name: "Curtain Call Ep16",
+      src: "ccs1%20%2816%29",
+      id: "vid_16"
+   }
+]
